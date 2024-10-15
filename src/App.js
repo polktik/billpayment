b299b1b28d9697f421c279e773057d4d2de9c2ed
polktik <polkrit2002@gmail.com>
2024-10-15 23:56:02 +0700

    welcome page finished

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Welcome from './pageJS/Welcome';
 import Login from './pageJS/Login';
+import Register from './pageJS/Register';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
--- src/pageJS/Welcome.js
+++ src/pageJS/Welcome.js
@@ -9,21 +9,33 @@ export default function Welcome() {
    const goLogin = () => {
       navigate("/login");
    };
+   const goRegister = () => {
+      navigate("/register");
+   };
 
-   function test() {
+   function WelcomePage() {
       return (
          <div>
             <div className="welcome-header">
                <button className="signin-button" onClick={goLogin}>Sign In</button>
             </div>
-            <img src={welcome} alt="welcome-pic" />
+            <div>
+               <img className="onlinePayment" src={welcome} alt="welcome-pic" />
+               <div className="welcome-text">
+                  <p className="first-line">Welcome to Bill payment Tracking System</p>
+                  <p className="second-line">Doesn't have an account?</p>
+               </div>
+            </div>
+            <div className="page-end">
+               <button className="register-button" onClick={goRegister}>Register Now</button>
+            </div>
          </div>
       );
    };
 
    return (
       <>
-         {test()}
+         {WelcomePage()}
       </>
    );
 }
