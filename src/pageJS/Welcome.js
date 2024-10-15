93729acce12047bf7eb7032e575463163a5daf75
polktik <polkrit2002@gmail.com>
2024-10-15 22:10:27 +0700

    test

---
+++ src/pageJS/Welcome.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import welcome from "../images/welcome-pic.jpg";
+import "../pageCSS/Welcome.css";
+
+export default function Welcome() {
+   const navigate = useNavigate();
+
+   const goLogin = () => {
+      navigate("/login");
+   };
+
+   function test() {
+      return (
+         <div>
+            <div className="welcome-header">
+               <button className="signin-button" onClick={goLogin}>Sign In</button>
+            </div>
+            <img src={welcome} alt="wselcome-pic" />
+         </div>
+      );
+   };
+
+   return (
+      <>
+         {test()}
+      </>
+   );
+}
